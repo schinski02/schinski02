4c95252c9219b7a0d79dee258907eba421b2600c
Jean-Pierre Kasperschinski <schinski_jp@Workplace.dyn6.pyur.net>
2023-11-24 18:10:10 +0100

    bisschn testen

---
+++ test.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner (System.in);
+		int a;
+		int b;
+		int c;
+		
+		System.out.println("Wert a");
+		a = s.nextInt();
+		
+		System.out.println("Wert b");
+		b = s.nextInt();
+		
+		c = a+b;
+		System.out.println(a +" + "+ b + " = "+ c);
+		
+		return;
+	}
+
+}
